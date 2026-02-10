0466b3aa78fd6e9048481bd56db4985e236623d8
UIT2024002 <tanaya.bhoyar@cumminscollege.in~>
2026-02-10 11:56:23 +0530

    Tanaya's Local changes

---
+++ Add.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Add{
+    public static void main(String arg[]){
+        System.out.println("Enter 2 Numbers:");
+        int a=s.nextInt();
+        int b=sc.nextInt();
+        System.out.prinltn("Addition= "+(a+b));
+    }
+}
